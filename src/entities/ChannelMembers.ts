05ef0ad69bc3bc59cb2619ce87efe10ab9f4fb8f
songcommit <commit@capa.ai>
2021-09-17 10:46:38 +0900

    typeorm entity

---
+++ src/entities/ChannelMembers.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Channels } from './Channels';
+import { Users } from './Users';
+
+@Index('UserId', ['UserId'], {})
+@Entity({ schema: 'sleact', name: 'channelmembers' })
+export class ChannelMembers {
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column('int', { primary: true, name: 'ChannelId' })
+  ChannelId: number;
+
+  //채널을 만든 사람의 id
+  @Column('int', { primary: true, name: 'UserId' })
+  UserId: number;
+
+  @ManyToOne(() => Channels, (channels) => channels.ChannelMembers, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'ChannelId', referencedColumnName: 'id' }])
+  Channel: Channels;
+
+  //연결된 사람들의 id
+  @ManyToOne(() => Users, (users) => users.ChannelMembers, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'UserId', referencedColumnName: 'id' }])
+  User: Users;
+}
